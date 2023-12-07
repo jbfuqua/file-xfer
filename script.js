97deb97e92633af71f0adf7351fb2e55d4b7fa2b
jbfuqua <joe_fuqua@yahoo.com>
2023-12-07 18:47:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ document.getElementById('uploadForm').addEventListener('submit', function(e) {
     const formData = new FormData();
     formData.append('file', file);
 
-    fetch('https://your-azure-function-url', {
+    fetch('vhttps://filexferfunction.azurewebsites.net/api/http_trigger?code=wp4NMqPf57kBtD7PLyQCE0yVKnQUj2tBF09PcZnMBjQeAzFuknRJtQ==', { // Replace with your Azure Function URL
         method: 'POST',
         body: formData
     })
     .then(response => response.text())
     .then(data => alert(data))
-    .catch(error => console.error('Error:', error));
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred while uploading the file.');
+    });
 });
