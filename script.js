d638d6e23f38413d877cf88ba490a462deef03e4
jbfuqua <joe_fuqua@yahoo.com>
2023-12-07 18:26:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('uploadForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+
+    fetch('https://your-azure-function-url', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => alert(data))
+    .catch(error => console.error('Error:', error));
+});
